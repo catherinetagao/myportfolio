5ecd13da280793008a67960be6b7533f8350a384
unknown <90160998+catherinetagao@users.noreply.github.com>
2025-03-21 16:41:48 +0000

    newww

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section class="grid grid-cols-1 gap-0 bg-blue-100 bg-opacity-25 md:grid-cols-2">
       <div class="flex flex-col items-start justify-center px-4 py-24 lg:px-20">
         <span class="mb-3 text-white bg-gray-900 rounded-lg p-2">
-          Undergraduate IT Student & Aspiring Web Developer
+           IT Graduate & Aspiring Web Developer
         </span>
         <h1 class="mb-6 text-4xl font-bold leading-tight text-{#000000}-900 md:text-4xl lg:text-5xl">
           Catherine Orbe
